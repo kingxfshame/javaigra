8eb29e1673accde77d3731b75bfb9c76655bbdab
kingxfshame <artur.kexpa@gmail.com>
2019-09-11 14:05:36 +0300

    na4alo

--- src/com/kingxfshame/GameWindow.java
+++ src/com/kingxfshame/GameWindow.java
@@ -3,6 +3,7 @@ package com.kingxfshame;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 
 public class GameWindow extends JFrame {
     private static GameWindow game_window;
@@ -11,7 +12,7 @@ public class GameWindow extends JFrame {
 	private static Image drop;
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
     	bg = ImageIO.read(GameWindow.class.getResourceAsStream("bg.jpg"));
 	game_window = new GameWindow(); // создали объект
 	game_window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // при закрытии программы будет завершаться код
@@ -23,10 +24,7 @@ public class GameWindow extends JFrame {
 	game_window.setVisible(true);
     }
     private static void onRepaint(Graphics g){
-    	g.setColor(Color.red);
-		g.fillOval(10,10,200,100);
-		g.setColor(Color.green);
-		g.drawLine(150,150,250,250);
+    		
 	}
 	private static class GameField extends JPanel{
     	@Override
